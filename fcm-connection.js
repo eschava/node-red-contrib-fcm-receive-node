5b06af9d15ad09c9fac7767451e868df3918cde8
Eugene Schava <eschava@gmail.com>
2020-03-24 09:53:30 +0200

    config node

---
+++ fcm-connection.js
@@ -0,0 +1,54 @@
+const { register, listen } = require('push-receiver');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function(RED) {
+    function FcmConnection(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+
+        node.name = n.name;
+        node.serverkey = n.serverkey;
+        node.callbacks = [];
+
+		(async () => {
+            var dir = path.join(RED.settings.userDir, 'fcm-receive/' + node.id);
+            var credFile = path.join(dir, 'fcm.cred');
+            var persistentIdsFile = path.join(dir, 'persistent.ids');
+
+            try {
+                fs.mkdirSync(dir, { recursive: true });
+            } catch (e) {}
+
+            var credentials;
+            try {
+                credentials = JSON.parse(fs.readFileSync(credFile));
+            } catch(e) {
+                credentials = await register(node.serverkey);
+                fs.writeFileSync(credFile, JSON.stringify(credentials));
+            }
+
+//            node.token = credentials.fcm.token;
+            node.log('token: ' + credentials.fcm.token);
+
+            credentials.persistentIds = [];
+            try {
+                credentials.persistentIds = fs.readFileSync(persistentIdsFile).toString().split("\n");
+                fs.unlinkSync(persistentIdsFile);
+            } catch(e) {}
+
+            listen(credentials, onNotification);
+
+            // Called on new notification
+            function onNotification({ notification, persistentId }) {
+              fs.appendFileSync(persistentIdsFile, persistentId + "\n");
+
+              var msg = {payload: notification, messageId: persistentId};
+              node.callbacks.forEach(function (callback){
+                   callback(msg);
+              });
+            }
+		})();
+    }
+    RED.nodes.registerType("fcm-connection", FcmConnection);
+}
--- fcm-receive.js
+++ fcm-receive.js
@@ -1,47 +1,19 @@
-const { register, listen } = require('push-receiver');
-const fs = require('fs');
-const path = require('path');
+const { listen } = require('push-receiver');
 
 module.exports = function(RED) {
     function FcmReceiveNode(config) {
         RED.nodes.createNode(this, config);
-        const serverKey = process.env.FCM_SERVER_KEY;
         var node = this;
 
-		(async () => {
-		    var dir = path.join(RED.settings.userDir, 'fcm-receive');
-		    var credFile = path.join(dir, 'fcm.cred');
-		    var persistentIdsFile = path.join(dir, 'persistent.ids');
-
-			try {
-				fs.mkdirSync(dir);
-			} catch (e) {}
-
-			var credentials;
-			try {
-				credentials = JSON.parse(fs.readFileSync(credFile));
-			} catch(e) {
-				credentials = await register(serverKey);
-				fs.writeFileSync(credFile, JSON.stringify(credentials));
-			}
-			
-			node.log('fcm-receive token: ' + credentials.fcm.token);
-			
-			var persistentIds = [];
-			try {
-				persistentIds = fs.readFileSync(persistentIdsFile).toString().split("\n");
-				fs.unlinkSync(persistentIdsFile);
-			} catch(e) {}
-			credentials.persistentIds = persistentIds;
-			listen(credentials, onNotification);
-
-			// Called on new notification
-			function onNotification({ notification, persistentId }) {
-			  var msg = {payload: notification, messageId: persistentId};
-			  node.send(msg);
-			  fs.appendFileSync(persistentIdsFile, persistentId + "\n");
-			}
-		})();
+        // Retrieve the config node
+        node.connection = RED.nodes.getNode(config.connection);
+        if (node.connection) {
+            node.connection.callbacks.push(
+                function(msg) {
+                    node.send(msg);
+                }
+            );
+        }
     }
     RED.nodes.registerType("fcm-receive", FcmReceiveNode);
 }
